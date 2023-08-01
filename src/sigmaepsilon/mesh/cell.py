fixed L2 shape function derivatives

src/sigmaepsilon/mesh/cell.py
@@ -358,7 +358,7 @@ class PolyCell(CellData):
             x = np.array(pcoords) if pcoords is not None else self.lcoords()
             dshp = self.shape_function_derivatives(x)
         if self.NDIM == 1:
-            jacobian_matrix_bulk_1d(dshp, ecoords)
+            return jacobian_matrix_bulk_1d(dshp, ecoords)
         else:
             return jacobian_matrix_bulk(dshp, ecoords)
 

src/sigmaepsilon/mesh/utils/cells/l2.py
@@ -47,7 +47,7 @@ def dshp_L2(r):
 @njit(nogil=True, parallel=True, cache=__cache)
 def dshp_L2_multi(pcoords: ndarray):
     nP = pcoords.shape[0]
-    res = np.zeros((nP, 2), dtype=pcoords.dtype)
+    res = np.zeros((nP, 2, 1), dtype=pcoords.dtype)
     for iP in prange(nP):
-        res[iP, :] = dshp_L2(pcoords[iP])
+        res[iP, :, 0] = dshp_L2(pcoords[iP])
     return res

tests/cells/test_lines.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+import numpy as np
+
 from sigmaepsilon.mesh import PolyData, PointData, CartesianFrame
 from sigmaepsilon.mesh.grid import grid
 from sigmaepsilon.mesh.cells import L2
@@ -22,9 +24,12 @@ class TestLineCells(unittest.TestCase):
         mesh = PolyData(pd, cd, frame=frame)
 
         jac = cd.jacobian_matrix()
-        pcoords = L2.lcoords()
+        self.assertTrue(isinstance(jac, np.ndarray))
+        
+        pcoords = L2.master_coordinates()
+        self.assertTrue(isinstance(pcoords, np.ndarray))
+        
         gdshp = L2.shape_function_derivatives(pcoords, jac=jac)
-
         self.assertTrue(
             gdshp.shape, (topo.shape[0], pcoords.shape[0], topo.shape[1], 1)
         )