a044136c8b963a0f1485704bb7cb1bb93c54477c
Bence Balogh <benceeok@gmail.com>
2023-08-08 22:53:45 +0200

    added test

--- tests/cells/test_interpolator.py
+++ tests/cells/test_interpolator.py
@@ -1,6 +1,8 @@
 import unittest
 import doctest
 
+import numpy as np
+
 from sigmaepsilon.core.testing import SigmaEpsilonTestCase
 import sigmaepsilon.mesh
 from sigmaepsilon.mesh.cells import H8, L3, Q4, Q9
@@ -34,6 +36,30 @@ class TestLagrangianCellInterpolator(SigmaEpsilonTestCase):
             values=source_values,
             target=target_coordinates,
         )
+        
+    def test_interpolator_H8_multi(self):
+        interpolator = H8.interpolator()
+        master_coordinates = H8.master_coordinates()
+
+        source_coordinates = master_coordinates / 2
+        target_coordinates = master_coordinates * 2
+        
+        source_values = np.random.rand(10, 2, 8)
+        shape = interpolator(
+            source=source_coordinates, 
+            values=source_values, 
+            target=target_coordinates[:3]
+        ).shape
+        self.assertEqual(shape, (10, 2, 3))
+        
+        source_values = np.random.rand(8, 2, 10)
+        shape = interpolator(
+            source=source_coordinates, 
+            values=source_values, 
+            target=target_coordinates[:3],
+            axis=0
+        ).shape
+        self.assertEqual(shape, (3, 2, 10))
         
     def test_interpolator_Q4_Q9(self):
         master_coordinates = Q9.master_coordinates()
