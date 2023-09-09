2e22a9d525344414ab9a11e94600dd947808c8ec
Bence Balogh <benceeok@gmail.com>
2023-09-09 22:26:37 +0200

    refactored

--- tests/test_index_manager.py
+++ tests/test_index_manager.py
@@ -6,14 +6,15 @@ from sigmaepsilon.mesh.indexmanager import IndexManager
 
 
 class TestIndexManager(unittest.TestCase):
-    def test_main(self):
+    def test_single(self):
         im = IndexManager()
         self.assertEqual(im.generate(1), 0)
         self.assertEqual(im.generate(1), 1)
         im.recycle(0)
         self.assertEqual(im.generate(1), 0)
         self.assertEqual(im.generate_np(1), 2)
-        
+    
+    def test_multi(self):
         im = IndexManager()
         self.assertEqual(im.generate(5), list(range(5)))
         im.recycle(list(range(5)))
