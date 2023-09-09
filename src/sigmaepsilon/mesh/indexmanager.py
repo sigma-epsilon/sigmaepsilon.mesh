8f2d43698f20121b47712c65d0ce1349d3123b8c
Bence Balogh <benceeok@gmail.com>
2023-09-09 22:37:34 +0200

    typing and docs

--- src/sigmaepsilon/mesh/indexmanager.py
+++ src/sigmaepsilon/mesh/indexmanager.py
@@ -7,21 +7,48 @@ from numpy import ndarray
 
 class IndexManager:
     """
-    Manages and index set by generating and recycling indices
-    of a set of points or cells.
+    Manages and index set by generating and recycling indices.
+    
+    Examples
+    --------
+    >>> from sigmaepsilon.mesh.indexmanager import IndexManager
+    >>> im = IndexManager()
+    >>> im.generate()
+    0
+    >>> im.generate(1)
+    1
+    >>> im.generate(2)
+    [2, 3]
+    >>> im.recycle(0)
+    >>> im.generate(2)
+    [0, 4]
+    >>> im.generate_np(2)
+    array([5, 6])
+    >>> im.recycle(5, 6)
+    >>> im.generate_np(2)
+    array([5, 6])
+    >>> im.recycle([5, 6])
+    >>> im.generate_np(2)
+    array([5, 6])
     """
 
-    def __init__(self, start=0):
+    def __init__(self, start:int=0):
         self.queue = []
         self.next = start
 
     def generate_np(self, n: int = 1) -> Union[int, ndarray]:
+        """
+        Generates indices as NumPy arrays.
+        """
         if n == 1:
             return self.generate(1)
         else:
             return np.array(self.generate(n))
 
     def generate(self, n: int = 1) -> Union[int, List[int]]:
+        """
+        Generates one or more indices.
+        """
         nQ = len(self.queue)
         if nQ > 0:
             if n == 1:
@@ -44,6 +71,9 @@ class IndexManager:
         return res
 
     def recycle(self, *args) -> None:
+        """
+        Recycles some indices.
+        """
         for a in args:
             if isinstance(a, Iterable):
                 self.queue.extend(a)
