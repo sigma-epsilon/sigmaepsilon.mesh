5ba25e77341a25412996872371b31c0ded8ea857
Bence Balogh <benceeok@gmail.com>
2023-10-04 20:58:38 +0200

    added imports

--- src/sigmaepsilon/mesh/plotting/__init__.py
+++ src/sigmaepsilon/mesh/plotting/__init__.py
@@ -1,3 +1,19 @@
 from .pvplot import pvplot
+from .mpl import (
+    triplot_mpl_hinton,
+    triplot_mpl_mesh,
+    triplot_mpl_data,
+    parallel_mpl,
+    aligned_parallel_mpl,
+    decorate_mpl_ax
+)
 
-__all__ = ["pvplot"]
+__all__ = [
+    "pvplot",
+    "triplot_mpl_hinton",
+    "triplot_mpl_mesh",
+    "triplot_mpl_data",
+    "parallel_mpl",
+    "aligned_parallel_mpl",
+    "decorate_mpl_ax",
+]
