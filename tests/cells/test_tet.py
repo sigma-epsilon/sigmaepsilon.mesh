00a9aa3c9b017a32121be25e5387967ff190517a
Bence Balogh <benceeok@gmail.com>
2023-12-01 21:24:31 +0100

    added new test

--- tests/cells/test_tet.py
+++ tests/cells/test_tet.py
@@ -64,6 +64,21 @@ class TestTet(unittest.TestCase):
         shpf(pcoords)
         shpmf(pcoords)
         dshpf(pcoords)
+        
+    def test_tet_surface(self):
+        Lx, Ly, Lz, nx, ny, nz = 1.0, 1.0, 1.0, 2, 2, 2
+        A = CartesianFrame(dim=3)
+        coords, topo, _ = triangulate(size=(Lx, Ly), shape=(nx, ny))
+        pd = PointData(coords=coords, frame=A)
+        cd = T3(topo=topo)
+        mesh2d = TriMesh(pd, cd)
+        mesh3d = mesh2d.extrude(h=Lz, N=nz)
+        
+        surface: TriMesh = mesh3d.surface(mesh_class=TriMesh)
+        normals = surface.normals()
+        
+        self.assertEqual(len(normals), len(surface.topology()))
+        self.assertTrue(surface.is_2d_mesh())
 
 
 class TestTET4(SigmaEpsilonTestCase):
