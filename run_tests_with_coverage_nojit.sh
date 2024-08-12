c286dd0dc3b71a0cbd1d73232eba790f325c769f
Bence Balogh <benceeok@gmail.com>
2024-08-12 19:03:02 +0200

    fixed coveragerc file name in test script

--- run_tests_with_coverage_nojit.sh
+++ run_tests_with_coverage_nojit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Disabling Numba JIT Compilation"
 export NUMBA_DISABLE_JIT=1
-./run_tests_with_coverage.sh
+poetry run pytest --cov-report html --cov-config=.coveragerc_nojit --cov sigmaepsilon.mesh
 echo "Enabling Numba JIT Compilation"
 export NUMBA_DISABLE_JIT=0
