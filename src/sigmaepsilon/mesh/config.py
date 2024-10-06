1c1264c31821a2bfb68c9d20fd1f4cab359f0769
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-10-06 14:03:17 +0200

    replaced dummy imports

--- src/sigmaepsilon/mesh/config.py
+++ src/sigmaepsilon/mesh/config.py
@@ -1,42 +1,15 @@
-try:
-    import vtk
+import importlib.util
 
-    __hasvtk__ = True
-except Exception:  # pragma: no cover
-    __hasvtk__ = False
 
-try:
-    import pyvista as pv
+def is_module_available(module_name):
+    return importlib.util.find_spec(module_name) is not None
 
-    __haspyvista__ = True
-except Exception:  # pragma: no cover
-    __haspyvista__ = False
 
-try:
-    import matplotlib as mpl
-
-    __hasmatplotlib__ = True
-except Exception:  # pragma: no cover
-    __hasmatplotlib__ = False
-
-try:
-    import plotly.express as px
-    import plotly.graph_objects as go
-
-    __hasplotly__ = True
-except Exception:  # pragma: no cover
-    __hasplotly__ = False
-
-try:
-    import networkx as nx
-
-    __hasnx__ = True
-except Exception:  # pragma: no cover
-    __hasnx__ = False
-
-try:
-    import k3d
-
-    __hask3d__ = True
-except Exception:  # pragma: no cover
-    __hask3d__ = False
+__hasvtk__ = is_module_available("vtk")
+__haspyvista__ = is_module_available("pyvista")
+__hasmatplotlib__ = is_module_available("matplotlib")
+__hasplotly__ = is_module_available("plotly.express") and is_module_available(
+    "plotly.graph_objects"
+)
+__hasnx__ = is_module_available("networkx")
+__hask3d__ = is_module_available("k3d")
