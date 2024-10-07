e77dfe5a78b1affd2bcc1d3756717f684315b052
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-10-07 19:19:59 +0200

    updated script files for testing

---
+++ run_tests_with_coverage.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set rc_path = .coveragerc
+
+if defined NUMBA_DISABLE_JIT (
+    set jit_value=%NUMBA_DISABLE_JIT%
+) else (
+    set jit_value=0
+)
+
+if "%1"=="NUMBA_DISABLE_JIT" (
+    echo Turning off Numba JIT.
+    set NUMBA_DISABLE_JIT=1
+    set rc_path = .coveragerc_nojit
+)
+
+set COVERAGE_RCFILE=%rc_path%
+
+poetry run coverage run --source=src -m pytest
+poetry run coverage xml
+poetry run coverage html
+
+set NUMBA_DISABLE_JIT=%jit_value%
--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-poetry run pytest --cov-report=html --cov-config=.coveragerc --cov=sigmaepsilon.mesh
+
+# Set default rc_path
+rc_path=".coveragerc"
+
+# Check if NUMBA_DISABLE_JIT is set, and if not, assign default value of 0
+if [[ -z "$NUMBA_DISABLE_JIT" ]]; then
+    jit_value=0
+else
+    jit_value=$NUMBA_DISABLE_JIT
+fi
+
+# Check if the first argument is "NUMBA_DISABLE_JIT"
+if [[ "$1" == "NUMBA_DISABLE_JIT" ]]; then
+    echo "Turning off Numba JIT."
+    export NUMBA_DISABLE_JIT=1
+    rc_path=".coveragerc_nojit"
+fi
+
+# Set the COVERAGE_RCFILE to the chosen rc_path
+export COVERAGE_RCFILE=$rc_path
+
+# Run the coverage commands using the specified .coveragerc file
+poetry run coverage run --source=src -m pytest
+poetry run coverage xml
+poetry run coverage html
+
+# Restore the original NUMBA_DISABLE_JIT value
+export NUMBA_DISABLE_JIT=$jit_value
