93ec681a98ed7d06535bc4a8f0a54441c163ffb7
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-12-12 22:11:52 +0100

    fix: set Windows-specific event loop policy for asyncio in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
 import matplotlib
 import pytest
+import asyncio
+import os
 
 
 @pytest.fixture(autouse=True)
 def set_mpl_backend():
     matplotlib.use("agg")
+
+
+# Windows specific settings
+if os.name == "nt":
+
+    @pytest.fixture(scope="session", autouse=True)
+    def set_event_loop_policy():
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
